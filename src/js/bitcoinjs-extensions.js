448eb191887d97def264b9f059d836f9011444a9
flo071 <flo071@gmx.at>
2020-10-27 12:26:10 +0100

    Noir[NOR] for Zcore Wallet

--- src/js/bitcoinjs-extensions.js
+++ src/js/bitcoinjs-extensions.js
@@ -1043,6 +1043,17 @@ libs.bitcoin.networks.nix = {
   wif: 0x80,
 };
 
+libs.bitcoin.networks.noir = {
+  messagePrefix: '\x18Noir Signed Message:\n',
+  bip32: {
+    public: 0x0488b21e,
+    private: 0x0488ade4,
+  },
+  pubKeyHash: 0x50,
+  scriptHash: 0x35,
+  wif: 0xD0,
+};
+
 libs.bitcoin.networks.neurocoin = {
   messagePrefix: '\x18PPCoin Signed Message:\n',
   bip32: {
